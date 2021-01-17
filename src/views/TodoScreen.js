539e5e07f3043c1330c8c5d632083284cc50b8de
Teofilo <danyel.285309@gmail.com>
2021-01-17 09:45:17 -0600

    fix: correction icons in main view

--- src/views/TodoScreen.js
+++ src/views/TodoScreen.js
@@ -6,10 +6,10 @@ import { Form } from '../components/Form';
 export const TodoScreen = () => {
     return (
         <div className='todo-screen-container'>
-            <i className='fas fa-check-square fa-5x'></i>
+            <i className='fas fa-calendar-check fa-5x'></i>
             <Card shadow={4} className="todo-card">
                 <div className='title'>
-                    <h3>Todo Productivity</h3>
+                    <h3><i className='fas fa-check-square'></i>Todo Productivity</h3>
                 </div>
                 <div className='form'>
                     <Form />
