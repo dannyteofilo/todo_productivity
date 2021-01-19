7304a26c1a1aeb1e2c36106075ae70f3bf5e3e84
Teofilo <danyel.285309@gmail.com>
2021-01-19 11:16:49 -0600

    fix: correction form and todolist component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,73 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useForm } from '../hooks/useForm';
-import { Textfield, Button } from 'react-mdl';
+import { Textfield, Button, Icon } from 'react-mdl';
+import { addTodo, fetchUpdateReset, updatetodo } from '../redux/actions/addTodo';
+import { useDispatch, useSelector } from 'react-redux';
 
 export const Form = () => {
 
-    const [formValues, handleInputChange] = useForm({
-        description: ''
-    })
+    const defaultForm = {
+        text: '',
+        timeSelect: '0',
+        isUpdate: false
+    }
+    const timeOpt = ['Small', 'Medium', 'Large']
 
-    const { description } = formValues;
+
+    const store = useSelector(store => store)
+    const dispatch = useDispatch();
+
+    const { update } = store;
+    const [form, setform] = useState({ defaultForm })
+    const [formValues, handleInputChange, reset] = useForm(defaultForm);
+    const { text, timeSelect } = formValues
+
+    useEffect(() => {
+        const { text, timeSelect } = formValues;
+        setform({ text, timeSelect })
+    }, [formValues])
+
+    useEffect(() => {
+        if (update) {
+            const { id, text, timeSelect } = update;
+            reset({ id, text, timeSelect, isUpdate: true });
+        }
+        return () => {
+            dispatch(fetchUpdateReset())
+        }
+    }, [update])
+
     const handleAdd = (e) => {
+        const { isUpdate, id } = formValues
         e.preventDefault();
-        console.log('e: ', e, formValues);
+        const data = { ...formValues, status: 'todo', date: new Date(), time: setTime() }
+        if (isUpdate) {
+            dispatch(updatetodo(id, data))
+        } else {
+            dispatch(addTodo(data))
+        }
+        reset();
+    }
+
+    const setTime = () => {
+        const hour = 60, small = 30, medium = 45, large = 60;
+        switch (form.timeSelect) {
+            case '0': {
+                return small * hour;
+            }
+            case '1': {
+                return medium * hour;
+            }
+
+            default: {
+                return large * hour;
+            }
+        }
+    }
+
+    const handleSelect = (e) => {
+        console.log('handle select: ', e);
+        handleInputChange(e);
     }
     return (
         <div className='form-container'>
@@ -19,9 +75,18 @@ export const Form = () => {
                 <Textfield
                     onChange={handleInputChange}
                     label="Text..."
-                    name='description'
-                    value={description}
+                    name='text'
+                    value={form.text ? form.text : text}
+                    required={true}
                 />
+                <div className='select'>
+                    <Icon name="timer" />
+                    <select name='timeSelect' value={form.timeSelect ? form.timeSelect : timeSelect} onChange={handleSelect}>
+                        {timeOpt.map((item, index) => {
+                            return <option key={index} value={index}>{item}</option>
+                        })}
+                    </select>
+                </div>
                 <Button type='submit' raised accent>Add</Button>
             </form>
         </div>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,8 +5,11 @@ import { List } from 'react-mdl';
 import { Todo } from './Todo';
 
 export const TodoList = () => {
-    const allTodos = useSelector(getTodos)
-    const todos = allTodos;
+    const allTodos = useSelector(getTodos);
+    const state = useSelector(state => state);
+    const { filter } = state;
+    const todos = filter === 'All' ? allTodos.filter(todo => (todo.status === 'done' || todo.status === 'todo')) :
+        filter === 'Completed' ? allTodos.filter(todo => todo.completed) : allTodos.filter(todo => !todo.completed);
     console.log('alltodos: ', allTodos);
     return (
         <ul className='todo-list'>
@@ -15,11 +18,14 @@ export const TodoList = () => {
                     <List style={{ width: '300px' }}>
                         {
                             todos.map((todo, index) => {
-                                return <Todo key={index} todo={todo} />
+                                return <Todo key={index} todo={todo} timer={false} remove={true} />
                             })
                         }
                     </List>
-                    : 'List empty'
+                    : <div className='list'>
+                        <span>List todo Empty </span>
+                        <i className='fas fa-list fa-4x'></i>
+                    </div>
             }
         </ul>
     )
