dbf89e4533e4d2b9abc0da644e0ba9cf509a1e8e
Teofilo <danyel.285309@gmail.com>
2021-01-21 23:03:19 -0600

    feat adding functionality to timer on todo component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { ListItem, Checkbox, ListItemAction, Icon, IconToggle } from 'react-mdl';
 import { useDispatch } from 'react-redux';
 import { toggleTodo, deleteTodo, setStatus, fetchUpdate } from '../redux/actions/addTodo';
 import { Timer } from './Timer';
 
-export const Todo = ({ todo, timer, remove }) => {
-    const { id, text, completed, status } = todo;
+export const Todo = ({ todo, timer, remove, update }) => {
+    const { id, text, completed, status, time } = todo;
     const dispatch = useDispatch();
-    const [play, setplay] = useState(false)
+    const [play, setPlay] = useState(false);
 
     const inProgress = 'inProgress';
     const onHold = 'onHold';
@@ -16,19 +16,32 @@ export const Todo = ({ todo, timer, remove }) => {
 
     const handleStart = (status) => {
         if (!completed) {
+            setPlay(true);
             dispatch(setStatus(id, status));
         }
     }
+
+    useEffect(() => {
+        console.log('status: ', status);
+        if (status === onHold) {
+            setPlay(false);
+        } else {
+            setPlay(true);
+        }
+    }, [status])
+
+    const onFinish = () => {
+        console.log('he finaizado');
+        dispatch(toggleTodo(id));
+    }
+
     return (
         <ListItem>
             <Checkbox label={text} ripple checked={completed} onChange={() => dispatch(toggleTodo(id))} />
             <ListItemAction>
                 {
                     timer &&
-                    <div className='todo-timer'>
-                        <Icon name="timer" />
-                        <Timer play={play} time={20} />
-                    </div>
+                    <Timer play={play} time={time} finish={onFinish} />
                 }
             </ListItemAction>
             <ListItemAction>
@@ -36,17 +49,12 @@ export const Todo = ({ todo, timer, remove }) => {
                     (status === onTodo || status === onHold) &&
                     <IconToggle ripple name="play_arrow" onClick={() => handleStart(inProgress)} />
                 }
-                {
-                    (status === inProgress && timer) &&
-                    <IconToggle ripple name="pause_arrow" onClick={() => handleStart(onHold)} />
-                }
-                {
-                    (status === onDone && timer) &&
-                    <IconToggle ripple name="restore" onClick={() => handleStart(inProgress)} />
-                }
             </ListItemAction>
             <ListItemAction>
-                <IconToggle ripple name="edit" onClick={() => dispatch(fetchUpdate(todo))} />
+                {
+                    update &&
+                    <IconToggle ripple name="edit" onClick={() => dispatch(fetchUpdate(todo))} />
+                }
             </ListItemAction>
             <ListItemAction>
                 {
