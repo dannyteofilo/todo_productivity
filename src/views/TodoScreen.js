8feb5dd9b774c570624a53eb33bef5a9f9593b01
Teofilo <danyel.285309@gmail.com>
2021-03-11 23:55:42 -0600

    fix: mobile view is implementated in form

--- src/views/TodoScreen.js
+++ src/views/TodoScreen.js
@@ -14,7 +14,7 @@ export const TodoScreen = () => {
     const allTodos = useSelector(getTodos);
     return (
         <div className='todo-screen-container'>
-            <i className='fas fa-calendar-check fa-5x'></i>
+            <i className='fas fa-calendar-check fa-4x'></i>
             <Card shadow={4} className="todo-card">
                 <div className='title'>
                     <h3><i className='fas fa-check-square'></i>Todo Productivity</h3>
